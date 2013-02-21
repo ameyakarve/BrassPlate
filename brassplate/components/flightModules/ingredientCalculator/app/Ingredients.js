f979d6f286baa52364a8ae6dca11f3cda4fbfa7d
Ameya Karve <ameya.karve@gmail.com>
2013-02-21 11:56:02 -0500

    Remove functionality added

--- brassplate/components/flightModules/ingredientCalculator/app/Ingredients.js
+++ brassplate/components/flightModules/ingredientCalculator/app/Ingredients.js
@@ -1,64 +1,77 @@
 define(
-    [
-        'flight/lib/component'
-    ],
-    function(component)
-    {
-        return component(Ingredients);
-        function Ingredients()
-        {
-            this.defaultAttrs({
-                selectedItems :[]
-              });
+['flight/lib/component', 'bootstrap/js/bootstrap-typeahead', 'mustache/mustache', 'text!templates/ingredientCalculator/addItem.txt', 'text!flightModules/ingredientCalculator/app/namelist.txt'],
+
+function(component, typeAhead, Mustache, addItemTemplate, nameList) {
+    return component(Ingredients);
+
+    function Ingredients() {
+        this.defaultAttrs({
+            selectedItems: []
+        });
 
 
-            this.initfunction = function()
-            {
-                require(
-                    [
-                        'text!flightModules/ingredientCalculator/app/namelist.txt',
-                        'bootstrap/js/bootstrap-typeahead'
-                    ],function(nameList, typeAhead){
-                        var names = (window.$.parseJSON(nameList));
-                        window.$("#ingredientTypeahead").typeahead({source:names, updater:function(item)
-                        {
-                            var index = names.indexOf(item);
-                            window.$("#ingredientsComponent").trigger({type:"newItemAdded",item:item,index:index});   
-                            return item;
-                        },matcher:function(item)
-                        {
-                            window.$("#warningGroup").removeClass("error");
-                            window.$("#inputWarning").html(" ");
-                            return true;
-                        }
-                        });
-                    }
-                );
-            };
-            this.itemAdded = function(event)
-            {
-                console.log(event.item, event.index,"New Item Added");
-                if(this.attr.selectedItems.indexOf(event.index)==-1)
-                {
-                    var id = "addedItem"+event.index;
-                    window.$("#itemList").append('<div class="row-fluid itemRow" id="'+id+'"></div>');
-                    window.$("#"+id).html('<div class="span8 itemName">'+event.item+'</div><div class="span2"><input class="input-block-level" type="number" value=0></div><div class="span2 itemValue">0.00</div>');
-                    this.attr.selectedItems.push(event.index);
-                    console.log(this.attr.selectedItems);    
+        this.initfunction = function() {
+
+            var names = (window.$.parseJSON(nameList));
+            window.$("#ingredientTypeahead").typeahead({
+                source: names,
+                updater: function(item) {
+                    var index = names.indexOf(item);
+                    window.$("#ingredientsComponent").trigger({
+                        type: "newItemAdded",
+                        item: item,
+                        index: index
+                    });
+                    return item;
+                },
+                matcher: function(item) {
+                    window.$("#warningGroup").removeClass("error");
+                    window.$("#inputWarning").html(" ");
+                    return true;
                 }
-                else
-                {
-                    window.$("#warningGroup").addClass("error");
-                    window.$("#inputWarning").html("This item is already present");
-                }
-                
-            };
-            
-            this.after("initialize",function()
-            {
-                this.initfunction();
-                this.on("newItemAdded",this.itemAdded);
             });
+
+        };
+        this.renderNewItem = function(name, index) {
+            return Mustache.render(addItemTemplate, {
+                name: name,
+                index: index
+            });
+        };
+        this.itemAdded = function(event) {
+            console.log(event.item, event.index, "New Item Added");
+            if (this.attr.selectedItems.indexOf(event.index) == -1) {
+                var id = "addedItem" + event.index;
+                window.$("#itemList").append(this.renderNewItem(event.item, event.index));
+                window.$("#removeItem"+event.index).click(function(){
+                    window.$("#ingredientsComponent").trigger({
+                        type:"itemRemoved",
+                        index:event.index
+                    });
+                });
+                this.attr.selectedItems.push(event.index);
+                console.log(this.attr.selectedItems);
+            }
+            else {
+                window.$("#warningGroup").addClass("error");
+                window.$("#inputWarning").html("This item is already present");
+            }
+
+        };
+        
+        this.itemRemoved = function(event)
+        {
+            console.log(event);
+            window.$("#addedItem"+event.index).remove();
+            var index = this.attr.selectedItems.indexOf(event.index);
+            this.attr.selectedItems.splice(index,1);
+            console.log(this.attr.selectedItems);
         }
+
+        this.after("initialize", function() {
+            this.initfunction();
+            this.on("newItemAdded", this.itemAdded);
+            this.on("itemRemoved",this.itemRemoved);
+        });
     }
-);
+});
--- brassplate/components/flightModules/ingredientCalculator/boot/init.js
+++ brassplate/components/flightModules/ingredientCalculator/boot/init.js
@@ -1,11 +1,28 @@
 define(
     [
-        'flightModules/ingredientCalculator/app/Ingredients'
+        'flightModules/ingredientCalculator/app/Ingredients',
+        'mustache/mustache',
+        'text!templates/ingredientCalculator/mainLeftPane.txt',
+        'text!templates/ingredientCalculator/sidebarTemplate.txt',
+        'text!templates/ingredientCalculator/footer.txt',
+        'text!templates/ingredientCalculator/hiddenstuff.txt'
         //all module definitions go here
     ],
-    function(Ingredients){
+    function(
+        Ingredients,
+        Mustache,
+        mainLeftPaneTemplate,
+        sidebarTemplate,
+        footerTemplate,
+        hiddenStuffTemplate
+    ){
         function initialize()
         {
+            console.log(mainLeftPaneTemplate);
+            window.$("#wrapper_inner").html(Mustache.render(mainLeftPaneTemplate,{}));
+            window.$("#sidebar").html(Mustache.render(sidebarTemplate,{}));    
+            window.$("#footer").html(Mustache.render(footerTemplate,{}));    
+            window.$("#footer").html(Mustache.render(hiddenStuffTemplate,{}));  
             Ingredients.attachTo("#ingredientsComponent");
             console.log("Booted app with all dependencies");
         }
